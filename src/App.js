2bdbe3a9c39e2b5e877d35cb36e94906d49032f4
Teedari <godfredderi47@gmail.com>
2022-04-10 02:51:58 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.scss';
+import "./index.css";
+import "antd/dist/antd.css";
+import "./App.scss";
+import Homepage from "./pages/user";
+import urls from "./utils/routes/page.routes";
+import SignIn from "./pages/common/signin";
+import SignUp from "./pages/common/signup";
+import ContactUs from "./pages/user/Contact";
+import BeatPage from "./pages/user/Beat";
+import BeatDetail from "./pages/user/BeatDetail";
+import Checkout from "./pages/user/Checkout";
+import Dashboard from "./pages/admin";
+import Genre from "./pages/admin/Genre";
+import Beat from "./pages/admin/Beat";
+import ListBeats from "./pages/admin/Beat/list.beat";
+import Users from "./pages/admin/Users";
+import Messages from "./pages/admin/Messages";
+import Settings from "./pages/admin/settings";
+import BeatUpdate from "./pages/admin/Beat/update.beat";
+import License from "./pages/admin/License";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={urls.homepage} element={<Homepage />} />
+        <Route path={urls.signin} element={<SignIn />} />
+        <Route path={urls.signup} element={<SignUp />} />
+        <Route path={urls.contact} element={<ContactUs />} />
+        <Route path={urls.beat} element={<BeatPage />} />
+        <Route path={urls.beat_detail} element={<BeatDetail />} />
+        <Route path={urls.checkout} element={<Checkout />} />
+        <Route index path={urls.dashboard} element={<Dashboard />} />
+        <Route path={urls.genre} element={<Genre />} />
+        <Route path={urls.dashboard_beat_create} element={<Beat />} />
+        <Route path={urls.dashboard_beat_list} element={<ListBeats />} />
+        <Route path={urls.dashboard_beat_update} element={<BeatUpdate />} />
+        <Route path={urls.dashboard_messages} element={<Messages />} />
+        <Route path={urls.dashboard_users} element={<Users />} />
+        <Route path={urls.dashboard_license} element={<License />} />
+        <Route path={urls.dashboard_settings} element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
