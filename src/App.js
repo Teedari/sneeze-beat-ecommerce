01139620a75434765ddcc655686390140a3b9642
Teedari <godfredderi47@gmail.com>
2022-04-10 03:12:42 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./index.css";
 import "antd/dist/antd.css";
 import "./App.scss";
+import "./main.css";
 import Homepage from "./pages/user";
 import urls from "./utils/routes/page.routes";
 import SignIn from "./pages/common/signin";
