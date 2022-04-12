111e0e841890cfdee30023ba44fadc79ec03ef00
Teedari <godfredderi47@gmail.com>
2022-04-12 13:20:06 +0000

    added category to beat basic form

--- src/pages/admin/Beat/forms/BeatBasicForm.jsx
+++ src/pages/admin/Beat/forms/BeatBasicForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from "react";
-import { Select, Form, Input, Button, Radio } from "antd";
+import { Select, Form, Input, Button, Radio, Row, Col } from "antd";
 import FormWrapper from "../../../../components/FormWrapper";
 import random from "random-number";
 import { useSelector } from "react-redux";
@@ -80,18 +80,34 @@ const BeatBasicForm = ({ ...props }) => {
           </Radio.Group>
         </Form.Item>
       )}
-      <Form.Item
-        name="genre"
-        label="Genre"
-        rules={[{ required: true, message: "Genre field is required" }]}>
-        <Select defaultValue="">
-          <Select.Option disabled>Select genre type</Select.Option>
-          <Select.Option value="hip pop">Hip pop</Select.Option>
-          <Select.Option value="drill">Drill</Select.Option>
-          <Select.Option value="dancehall">Dance Hall</Select.Option>
-          <Select.Option value="raggae">Raggae</Select.Option>
-        </Select>
-      </Form.Item>
+      <Row gutter={[14]}>
+        <Col md={12}>
+          <Form.Item
+            name="genre"
+            label="Genre"
+            rules={[{ required: true, message: "Genre field is required" }]}>
+            <Select defaultValue="">
+              <Select.Option disabled>Select genre type</Select.Option>
+              <Select.Option value="hip pop">Hip pop</Select.Option>
+              <Select.Option value="drill">Drill</Select.Option>
+              <Select.Option value="dancehall">Dance Hall</Select.Option>
+              <Select.Option value="raggae">Raggae</Select.Option>
+            </Select>
+          </Form.Item>
+        </Col>
+        <Col md={12}>
+          <Form.Item
+            name="category"
+            label="Category"
+            rules={[{ required: true, message: "Category field is required" }]}>
+            <Select defaultValue="">
+              <Select.Option disabled>Select Category</Select.Option>
+              <Select.Option value="recommended">Recommended</Select.Option>
+              <Select.Option value="trending">Trending</Select.Option>
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
       <Form.Item
         name="bpm"
         label="Beat Per Minute(bpm)"
@@ -99,7 +115,8 @@ const BeatBasicForm = ({ ...props }) => {
         rules={[{ required: true, message: "BPM field is required" }]}>
         <Input type="number" />
       </Form.Item>
-      <div className="flex md:gap-2 md:flex-row flex-col ">
+      <Row gutter={[14]}>
+        <Col sm={24} md={12}>
         <Form.Item
           name="general_price"
           label="MP3 (Price)"
@@ -107,6 +124,9 @@ const BeatBasicForm = ({ ...props }) => {
           rules={[{ required: true, message: "Price field is required" }]}>
           <Input type="number" />
         </Form.Item>
+        </Col>
+        <Col sm={24} md={12}>
+
         <Form.Item
           name="exclusive_price"
           label="Exclusive (Price)"
@@ -114,10 +134,14 @@ const BeatBasicForm = ({ ...props }) => {
           rules={[{ required: true, message: "Price field is required" }]}>
           <Input type="number" />
         </Form.Item>
-      </div>
+        </Col>
+      </Row>
       <div className="flex justify-end">
         {props.createForm && (
-          <Button htmlType="submit" className="btn btn-primary" loading={beatState.sendingState === HttpStatus.PENDING}>
+          <Button
+            htmlType="submit"
+            className="btn btn-primary"
+            loading={beatState.sendingState === HttpStatus.PENDING}>
             Continue
           </Button>
         )}
