dbc5d2191e713c79c9afcb581e996f741309dc92
Teedari <godfredderi47@gmail.com>
2022-04-19 23:28:48 +0000

    implemented protected routes

--- src/App.js
+++ src/App.js
@@ -41,12 +41,12 @@ function App() {
           <Route index path={'/dashboard/*'} element={<AdminOnlyRoutes allowered_users={[UserProfile.ADMIN_ROLE, UserProfile.USER_ROLE]} />} />
           {/* <Route index path={urls.dashboard} element={<Dashboard />} /> */}
           {/* <Route path={urls.genre} element={<Genre />} /> */}
-          <Route path={urls.dashboard_beat_create} element={<Beat />} />
-          <Route path={urls.dashboard_beat_list} element={<ListBeats />} />
-          <Route path={urls.dashboard_beat_update} element={<BeatUpdate />} />
-          <Route path={urls.dashboard_messages} element={<Messages />} />
-          <Route path={urls.dashboard_users} element={<Users />} />
-          <Route path={urls.dashboard_license} element={<License />} />
+          {/* <Route path={urls.dashboard_beat_create} element={<Beat />} /> */}
+          {/* <Route path={urls.dashboard_beat_list} element={<ListBeats />} /> */}
+          {/* <Route path={urls.dashboard_beat_update} element={<BeatUpdate />} /> */}
+          {/* <Route path={urls.dashboard_messages} element={<Messages />} /> */}
+          {/* <Route path={urls.dashboard_users} element={<Users />} /> */}
+          {/* <Route path={urls.dashboard_license} element={<License />} /> */}
           <Route path={urls.dashboard_settings} element={<Settings />} />
         </Route>
       </Routes>
